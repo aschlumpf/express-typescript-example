b4bf447d30fb3552be851e08f48379b473798fbf
Alex Schlumpf <alex.schlumpf@icims.com>
2021-04-13 17:33:07 -0400

    jest tests

---
+++ src/test/index.spec.ts
@@ -0,0 +1,445 @@
+const promiseTest = (): Promise<number> =>
+  new Promise((resolve, reject) => {
+    resolve(1);
+  });
+
+describe('Example async/await test', () => {
+  xit('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+  it('awaits promise', async () => {
+    const x = await promiseTest();
+    expect(x).toEqual(1);
+    expect(x).toEqual(1);
+    // assert.equal(x, 1);
+  });
+});
